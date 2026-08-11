dc2440b4846f7e518767bafb30074fdbdc180c58
Lorenzo <lorenzo_tomasello@virgilio.it>
2026-08-11 16:43:46 +0200

    php_01_nome_cognome

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$integer = 10;
+
+$float = 10.5;
+
+$string = "Stringa di prova";
+
+$boolean = false;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+const NOME = "Donato";
+
+echo NOME;
+
+NOME = "Alberto";
+
+echo NOME;
